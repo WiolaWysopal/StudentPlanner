8088d8914a5c4e31f720f254cdd38b0f96e7c01f
Wioletta Wysopal <wioletta.wysopal@gmail.com>
2026-09-16 14:29:22 +0200

    feat: create navigation between main and add task activities

--- app/src/main/java/com/wiolawysopal/studentplanner/AddTaskActivity.java
+++ app/src/main/java/com/wiolawysopal/studentplanner/AddTaskActivity.java
@@ -8,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.widget.Button;
-import android.content.Intent;
-
-public class MainActivity extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button addTaskButton = findViewById(R.id.addTaskButton);
-        addTaskButton.setOnClickListener(v -> {
-           Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
-           startActivity(intent);
-        });
+        setContentView(R.layout.activity_add_task);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
