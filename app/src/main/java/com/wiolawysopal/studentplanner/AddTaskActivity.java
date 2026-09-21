c09579fca60b28f6eebdfb2211d4d03219c1bb09
Wioletta Wysopal <wioletta.wysopal@gmail.com>
2026-09-21 15:03:11 +0200

    feat: distinguish task editing mode

--- app/src/main/java/com/wiolawysopal/studentplanner/AddTaskActivity.java
+++ app/src/main/java/com/wiolawysopal/studentplanner/AddTaskActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import android.content.Intent;
 
@@ -23,11 +24,14 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_task);
+        TextView addTaskTitleTextView = findViewById(R.id.addTaskTitleTextView);
         EditText taskTitleEditText = findViewById(R.id.taskTitleEditText);
         Button saveTaskButton = findViewById(R.id.saveTaskButton);
         String existingTaskTitle = getIntent().getStringExtra(EXTRA_EDIT_TASK_TITLE);
         if (existingTaskTitle != null) {
             taskTitleEditText.setText(existingTaskTitle);
+            addTaskTitleTextView.setText(R.string.edit_task_title);
+            saveTaskButton.setText(R.string.save_changes);
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
