d4084ef27e433e02c3bd5a89ea84022303581bed
Wioletta Wysopal <wioletta.wysopal@gmail.com>
2026-09-25 11:59:25 +0200

    chore: bump database version for task completion

--- app/src/main/java/com/wiolawysopal/studentplanner/AppDatabase.java
+++ app/src/main/java/com/wiolawysopal/studentplanner/AppDatabase.java
@@ -3,7 +3,7 @@ package com.wiolawysopal.studentplanner;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Task.class}, version=3)
+@Database(entities = {Task.class}, version=4)
 public abstract class AppDatabase extends RoomDatabase{
     public abstract TaskDao taskDao();
 }
